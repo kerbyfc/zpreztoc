04775c780b67ad15963204b98cc28597c973488e
kerbyfc <kerbyfc@gmail.com>
2014-11-19 18:09:32 +0300

    Update .install.sh

--- .install.sh
+++ .install.sh
@@ -1,6 +1,6 @@
 #!/bin/zsh
 
-[[ ! -d "prezto" ]] && git clone --recursive https://github.com/sorin-ionescu/prezto.git "${ZDOTDIR:-$HOME}/.zprezto"
+[[ ! -d "~/.zprezto" ]] && git clone --recursive https://github.com/sorin-ionescu/prezto.git "${ZDOTDIR:-$HOME}/.zprezto"
 
 setopt EXTENDED_GLOB
 for rcfile in "${ZDOTDIR:-$HOME}"/.zprezto/runcoms/^README.md(.N); do
@@ -11,6 +11,6 @@ mkdir -p $HOME/.zsh.before
 mkdir -p $HOME/.zsh.after
 mkdir -p $HOME/.zsh.prompts
 
-[[ ! -d "zpreztoc" ]] && git clone --recursive https://github.com/kerbyfc/zpreztoc.git "${ZDOTDIR:-$HOME}/.zprezto/zpreztoc"
+[[ ! -d "~/.zprezto/zpreztoc" ]] && git clone --recursive https://github.com/kerbyfc/zpreztoc.git "${ZDOTDIR:-$HOME}/.zprezto/zpreztoc"
 
-ln -nfs "$HOME/.zprezto/zpreztoc/zpreztorc" "${ZDOTDIR:-$HOME}/.zpreztorc"
+# ln -nfs "$HOME/.zprezto/zpreztoc/zpreztorc" "${ZDOTDIR:-$HOME}/.zpreztorc"
